b2a5058efccc60cd6f28e8aebbfe233ecf6fa88d
Lyreplus <lyreplus@gmail.com>
2024-11-26 21:51:44 +0100

    fix: add case with hours less than 1

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,6 @@
 def prettify_minutes(hours):
     hours_truncated = int(hours)
     minutes = int((hours-hours_truncated)*60)
+    if hours_truncated < 1:
+        return f"{minutes} minuti"
     return f"{hours_truncated} ore e {minutes} minuti"
